df4c1a91edbe80c7bf494e47b97c8d7b395fc6e9
EnriqueDarioGonzalez <enrii.xd6@gmail.com>
2023-08-15 13:05:01 -0300

    update3

--- JS/main.js
+++ JS/main.js
@@ -1,46 +1,180 @@
-const Producto = function(nombre, precio, stock){
+class Producto {
+  constructor(nombre, precio, cantidad) {
     this.nombre = nombre;
     this.precio = precio;
-    this.stock = stock;
+    this.cantidad = cantidad;
   }
-
-
-  let producto1 = new Producto("Bateria", 550000, 5)
-  let producto2 = new Producto("Guitarra", 145000, 12)
-  let producto3 = new Producto("Trompeta", 180000, 4)
-  let producto4 = new Producto("Teclado", 235000, 12)
-  let producto5 = new Producto("Violin", 980000, 9)
-
-
-  let lista = [producto1, producto2, producto3, producto4, producto5]
-
-
-  function filtrarProducto(){
-    let palabraClave = prompt("¿Qué producto esta buscando?")
-    let resultado = lista.filter ( (producto)=> producto.nombre.includes(palabraClave)  )
-    if (resultado.length > 0){
-      console.table(resultado)
-    }else{
-      alert("No se encontro ningun articulo con el nombre: " + palabraClave)
-    }
-  }
-
-
-let boton1 = document.getElementById("filter")
-boton1.addEventListener("click", filtrarProducto)
-
-function agregarProducto() {
-  let nombre = prompt("Ingresa el nombre del producto para agregar al stock")
-  let precio = parseInt(prompt("Ingresa el precio del producto"))
-  let stock = parseInt(prompt("Ingresa la cantidad de productos adicionados al stock"))
-  if( nombre === "" || isNaN(precio)  || isNaN(stock)){
-    alert("Ingrese valores válidos")
-    return
-  }
-  let producto = new Producto(nombre,precio,stock)
-  lista.push(producto)
-  console.table(lista)
 }
 
-let boton2 = document.getElementById("addstock")
-boton2.addEventListener("click", agregarProducto)
+document.addEventListener('DOMContentLoaded', () => {
+  const productosGuardados = JSON.parse(localStorage.getItem('productos')) || [];
+  inventario = productosGuardados;
+});
+
+let inventario = [];
+
+function buscarProducto() {
+  const searchTerm = document.getElementById('searchInput').value;
+  const productoEncontrado = inventario.find(producto => producto.nombre.toLowerCase() === searchTerm.toLowerCase());
+
+  if (productoEncontrado) {
+    Swal.fire({
+      icon: 'success',
+      title: 'Producto Encontrado',
+      html: `<strong>Nombre:</strong> ${productoEncontrado.nombre}<br><strong>Precio:</strong> $${productoEncontrado.precio}<br><strong>Cantidad:</strong> ${productoEncontrado.cantidad}`,
+      confirmButtonText: 'Cerrar'
+    });
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Producto no encontrado',
+      text: 'El producto no se encuentra en el inventario.'
+    });
+  }
+}
+
+function agregarStock() {
+  const nombre = document.getElementById('nombreProducto').value;
+  const precio = parseFloat(document.getElementById('precioProducto').value);
+  const cantidad = parseInt(document.getElementById('cantidadProducto').value);
+
+  if (!nombre || isNaN(precio) || isNaN(cantidad)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Por favor, completa todos los campos correctamente.'
+    });
+    return;
+  }
+
+  const nuevoProducto = new Producto(nombre, precio, cantidad);
+  inventario.push(nuevoProducto);
+  actualizarInventario();
+
+  Swal.fire({
+    icon: 'success',
+    title: 'Éxito',
+    text: 'Stock agregado correctamente.'
+  });
+}
+
+
+function actualizarInventario() {
+  localStorage.setItem('productos', JSON.stringify(inventario));
+}
+
+
+function actualizarInventario() {
+  localStorage.setItem('productos', JSON.stringify(inventario));
+  mostrarInventario();
+}
+
+function mostrarInventario() {
+  const inventarioList = document.getElementById('inventarioList');
+  inventarioList.innerHTML = '';
+
+  inventario.forEach(producto => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `<strong>${producto.nombre}</strong> - Precio: $${producto.precio} - Cantidad: ${producto.cantidad}`;
+    inventarioList.appendChild(listItem);
+  });
+}
+
+function eliminarProducto(nombre) {
+  const indice = inventario.findIndex(producto => producto.nombre === nombre);
+
+  if (indice !== -1) {
+    inventario.splice(indice, 1);
+    actualizarInventario();
+    Swal.fire({
+      icon: 'success',
+      title: 'Producto Eliminado',
+      text: 'El producto ha sido eliminado del inventario.'
+    });
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Producto no encontrado',
+      text: 'El producto no se encuentra en el inventario.'
+    });
+  }
+}
+
+
+function mostrarInventario() {
+  const inventarioList = document.getElementById('inventarioList');
+  inventarioList.innerHTML = '';
+
+  inventario.forEach(producto => {
+    const listItem = document.createElement('li');
+    listItem.className = 'dark-text';
+    listItem.innerHTML = `
+      <strong>${producto.nombre}</strong> - Precio: $${producto.precio} - Cantidad: ${producto.cantidad}
+      <button class="eliminar-button" onclick="eliminarProducto('${producto.nombre}')">Eliminar</button>
+      <button class="editar-button" onclick="editarProducto(${producto.id})">Editar</button>`;
+    inventarioList.appendChild(listItem);
+  });
+}
+
+function editarProducto(id) {
+const producto = inventario.find(item => item.id === id);
+
+if (!producto) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Producto no encontrado',
+    text: 'El producto no se encuentra en el inventario.'
+  });
+  return;
+}
+
+Swal.fire({
+  title: `Editar Producto - ${producto.nombre}`,
+  html: `
+    <label for="editarPrecio">Nuevo Precio:</label>
+    <input id="editarPrecio" type="number" step="0.01" value="${producto.precio}" class="swal2-input">
+    <label for="editarCantidad">Nueva Cantidad:</label>
+    <input id="editarCantidad" type="number" value="${producto.cantidad}" class="swal2-input">`,
+  focusConfirm: false,
+  customClass: {
+    popup: 'swal2-edit-product'
+  },
+  preConfirm: () => {
+      const nuevoPrecio = parseFloat(document.getElementById('editarPrecio').value);
+      const nuevaCantidad = parseInt(document.getElementById('editarCantidad').value);
+
+      if (isNaN(nuevoPrecio) || isNaN(nuevaCantidad)) {
+        Swal.showValidationMessage('Por favor, ingrese valores numéricos válidos.');
+      }
+
+      producto.precio = nuevoPrecio;
+      producto.cantidad = nuevaCantidad;
+      actualizarInventario();
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Producto Editado',
+        text: 'El producto ha sido editado en el inventario.'
+      });
+    }
+  });
+}
+
+
+// Cargar productos almacenados en localStorage al iniciar la página
+document.addEventListener('DOMContentLoaded', () => {
+  const productosGuardados = JSON.parse(localStorage.getItem('productos')) || [];
+  inventario = productosGuardados;
+  mostrarInventario();
+});
+
+
+//botonera
+const buscarBtn = document.getElementById("buscarproducto");
+buscarBtn.addEventListener("click", () => {
+  buscarProducto();
+});
+const agregarBtn = document.getElementById("addStock");
+agregarBtn.addEventListener("click", () => {
+  agregarStock();
+});
